4940f91f3816463969bb06636e46bdc89fa731e6
Adithi <63908022+adithi-su@users.noreply.github.com>
2022-05-29 11:10:17 +0530

    Update 1to9custom.c

--- Day2/1to9custom.c
+++ Day2/1to9custom.c
@@ -5,7 +5,6 @@ int main(){
 int result = 0, count = 9;
 result = load(0x0,count+1);
 printf("sum of %d numbers is %d", count, result);
-return 0;
 }
 
 // while compiling in RISC V, use riscv64-unknown-elf-gcc -Ofast -mabi=lp64 -march=rv64i -o <object filename> <C Program filename> <assemby lang file>
